f4ef13a69543c1fcfaea095898231c578a4a6716
Sébastien Fabié <sebastienfabie@gmail.com>
2026-05-11 17:56:45 +0200

    fix(api): add CORS configuration and update env example (#12)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import 'dotenv/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService<unknown, boolean> = app.get(ConfigService);
   const isDev: boolean =
     configService.get<string>('NODE_ENV') === 'development';
+
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development' ? '*' : process.env.ALLOWED_ORIGIN, // En dev on accepte tout
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
